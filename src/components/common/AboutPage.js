73cddb99b629570c8a2d50749e9422e03373a9e1
Cihan <cihannisan@gmail.com>
2017-01-20 15:58:40 -0800

    Created Home page and About Page

---
+++ src/components/common/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+
+  return (
+
+    <div>
+      <h1>ABOUT PAGE</h1>
+      <p>paragraphparagraphparagraphparagraphparagraphparagraph</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/common/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+
+    <div>
+      <h1>HOME PAGE</h1>
+      <p> paragraphparagraphparagraphparagraphparagraphparagraphparagraphparagraph </p>
+    </div>
+  );
+};
+
+export default Home;

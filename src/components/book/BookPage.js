d3af3e9ef8ea8968c5c664fb201e5cd16065526a
Cihan <cihannisan@gmail.com>
2017-01-20 17:16:43 -0800

    Create book page that displays book list

--- src/components/book/BookPage.js
+++ src/components/book/BookPage.js
@@ -7,17 +7,37 @@ class Book extends React.Component {
   }
 
   submitBook(input){
+    alert(input + 'Book Submitted');
+  }
+
+  handleInput(e){
 
     e.preventDefault();
+    const input = e.target.value;
+    this.submitBook(input);
 
-    alert('Book Submitted');
+    e.target.reset();
   }
 
   render() {
     return (
       <div>
-        <h3>Books</h3>
+        <div>
+          <h3>Books</h3>
+          <ul>
+            {this.props.books.map((book,index) => <li key={index}>{book.title}</li>)}
+          </ul>
+        </div>
+        <div>
+          <h3>Book Entry Form</h3>
+          <form onSubmit={()=> this.handleInput()}>
+            <input type="text" />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
 }
+
+export default Book;

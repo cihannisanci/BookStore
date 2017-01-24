d09d8e7ef65d7b1637cc658be15519d198fe59c9
Cihan <cihannisan@gmail.com>
2017-01-23 16:23:09 -0800

    Add reducer

---
+++ src/reducers/bookReducer.js
@@ -0,0 +1,16 @@
+
+export default (state=[], action) => {
+
+  switch(action.type){
+
+    case 'CREATE_BOOK':
+      return [
+        ...state,
+        Object.assign({}, action.book);
+      ];
+
+    default:
+      return state;
+  }
+
+};

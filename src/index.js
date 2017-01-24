c29e456572008ea953fbdb8ff7255fc891f4985d
Cihan <cihannisan@gmail.com>
2017-01-23 16:21:35 -0800

    Render app in index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 // Set up your application entry point here...
+import React from 'react';
+import {Provider} from 'react-redux';
+import ReactDOM from 'react-dom';
+import {Route, Router, IndexRoute, browserHistory} from 'react-router';
+import Home from './components/common/HomePage';
+import About from './components/common/AboutPage';
+import App from './components/App';
+import Book from './components/book/BookPage';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+ReactDOM.render((
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/books' component={Book} />
+      </Route>
+    </Router>
+  </Provider>
+), document.getElementById('app'));
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Route, Router, IndexRoute, browserHistory} from 'react-router';
-import Home from './components/common/HomePage';
-import About from './components/common/AboutPage';
-import App from './components/App';
-import Book from './components/book/BookPage';
+// import 'babel-polyfill';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {Route, Router, IndexRoute, browserHistory} from 'react-router';
+// import Home from './components/common/HomePage';
+// import About from './components/common/AboutPage';
+// import App from './components/App';
+// import Book from './components/book/BookPage';
 
-ReactDOM.render((
+// ReactDOM.render((
 
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/books' component={Book} />
-    </Route>
-  </Router>
-), document.getElementById('app'));
+//   <Router history={browserHistory}>
+//     <Route path='/' component={App}>
+//       <IndexRoute component={Home} />
+//       <Route path='/about' component={About} />
+//       <Route path='/books' component={Book} />
+//     </Route>
+//   </Router>
+// ), document.getElementById('app'));
